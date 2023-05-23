7d66af354ca2fa6d0c1e0e6b5e7149fe952b28b6
Illia1111 <129652550+Illia1111@users.noreply.github.com>
2023-05-23 22:59:21 +0300

    Add files via upload
    добавив сюрікени на кнопку f,   а також коли іду вправо та вліво і нажимаю на кнопки g та h виконується удар відповідно до сторони в яку іду (удар виконується тільки коли рухаюсь)

--- mortal_combat.py
+++ mortal_combat.py
@@ -4,8 +4,9 @@ from pygame import *
 
 
 img_back = "back.jpg"  
-img_hero = "right1.png"  
-img_enemy = "left.png"  
+img_hero = "left1.png"  
+img_enemy = "right.png"  
+
 
 class GameSprite(sprite.Sprite):
     
@@ -17,7 +18,8 @@ class GameSprite(sprite.Sprite):
         self.image_r = transform.scale(image.load('right1.png'), (size_x, size_y))
         self.image_rr = transform.scale(image.load('right.png'), (size_x, size_y))
         self.image_ll = transform.scale(image.load('left.png'), (size_x, size_y))
-        
+        self.image_kk = transform.scale(image.load('kick2.png'), (size_x, size_y))
+        self.image_k = transform.scale(image.load('kick3.png'), (size_x, size_y))
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
@@ -28,11 +30,19 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+    def fire(self):
+        bullet = Bulet('suriken.png',self.rect.centerx,self.rect.top,50,40,10 )
+        bullets.add(bullet)
+
+    def update(self):
+        self.rect.y = self.rect.y + self.speed
+
+
 
     def jump(self):
         if self.isJump:
             if self.jumpCount >= -10: 
-                self.rect.y -= (self.jumpCount * abs(self.jumpCount)) * 0.5
+                self.rect.y -= (self.jumpCount * abs(self.jumpCount)) * 0.3
                 self.jumpCount -= 1
             else:
                 self.isJump = False
@@ -61,22 +71,24 @@ class Enemy(GameSprite):
         if keys[K_a]:
             self.image  = self.image_ll
             self.rect.x = self.rect.x-self.speed
+            if keys[K_g]:
+                self.image  = self.image_k
         if keys[K_d]:
             self.image  = self.image_rr
             self.rect.x = self.rect.x+self.speed
-    def kick(self):
-        keys = key.get_pressed()
-        if keys[K_g]:
-            pass
-           
+            if keys[K_h]:
+                self.image  = self.image_kk
 
+class Bulet(GameSprite):
+    def update(self):
+        self.rect.x = self.rect.x - self.speed
+        if self.rect.x < 0 :
+            self.kill()
 
+score = 0
+lost = 0
 
-
-
-
-
-
+bullets = sprite.Group()
 
 win_width = 700
 win_height = 500
@@ -90,8 +102,10 @@ run = True
 clock = time.Clock()
 FPS = 60
 
-enemy = Enemy(img_enemy,550,100,150,200,5)
-hero = Player(img_hero,10,100,120,200,5)
+enemy = Enemy(img_enemy,10,100,150,200,5)
+hero = Player(img_hero,550,100,120,200,5)
+
+
 
 font.init()
 font = font.SysFont('Arial',24)
@@ -107,18 +121,35 @@ while run:
         if e.type == QUIT:
             run = False
         elif e.type == KEYDOWN:
-            if e.key == K_SPACE:
+            if e.key == K_UP:
                 hero.isJump = True
+            if e.key == K_w:
+                enemy.isJump = True
+            if e.key == K_f:
+                hero.fire()
             
     window.blit(background, (0, 0))
 
     enemy.reset()
     enemy.move()
+    enemy.jump()
+    
+
     hero.jump()
     hero.reset()
     hero.move()
     
+    bullets.update()
+    bullets.draw(window)
+
+    #collides = sprite.groupcollide(enemy, bullets, True, True)
     
+
+
+
+
+
+
 
 
 
